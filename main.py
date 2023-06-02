f0e3c6ce367491c4115f51ee8786b26b633e089c
Fernando Dalai <ferdalaixd@gmail.com>
2023-06-02 01:01:00 -0600

    Create Models

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+
+from database import User
+from database import Movie
+from database import UserReview
+
 from database import database as connection
+
 
 #Start the server: uvicorn main:app
 
@@ -12,14 +18,14 @@ def startup():
     if connection.is_closed():
         connection.connect()
 
-        print('Connecting...')
+    connection.create_tables([User, Movie, UserReview])
 
 @app.on_event('shutdown')
 def shutdown():
     if not connection.is_closed():
         connection.close()
 
-        print('Close')
+
 
 @app.get('/')
 async def index():
