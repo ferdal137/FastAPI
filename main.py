b98f289e94988e8d3ee82e97baf86c03e79963b5
Fernando Dalai <ferdalaixd@gmail.com>
2023-06-01 23:25:46 -0600

    Events

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ app = FastAPI(title = 'Project Movie Review',
             description = 'In this project we create a web app to share movie reviews',
             version = '1')
 
+@app.on_event('startup')
+def startup():
+    print('The server is starting')
+
+@app.on_event('shutdown')
+def shutdown():
+    print("The server is ending")
 
 @app.get('/')
 async def index():
     return 'Hello world from fastAPI server'
 
-
-@app.get('/about')
-async def about():
-    return 'About'
